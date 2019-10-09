3e99992d2447db4f65abb18f878ed6ea360c0d49
cildhdi <miles_yang@foxmail.com>
2019-10-09 19:28:09 +0800

    codewars setup

---
+++ common.h
@@ -0,0 +1,8 @@
+#include <igloo/igloo.h>
+#include <igloo/igloo_alt.h>
+#include <igloo/igloo_framework.h>
+#include <iostream>
+
+using namespace igloo;
+
+int main(int argc, char *argv[]) { return TestRunner::RunAllTests(argc, argv); }
